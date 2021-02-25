f8159e85c44f76a6e47cb65e3f3578b495e888df
Unai Orbe <uorbe001@gmail.com>
2021-02-25 11:55:31 +0100

    Disable some heavy rules I don't need

--- keyboards/keebio/iris/keymaps/uorbe001/rules.mk
+++ keyboards/keebio/iris/keymaps/uorbe001/rules.mk
@@ -3,3 +3,5 @@ BACKLIGHT_ENABLE = yes
 COMMAND_ENABLE   = no  # Commands for debug and configuration
 MOUSEKEY_ENABLE = no
 UNICODE_ENABLE = yes
+EXTRAKEY_ENABLE = no       # Audio control and System control
+ENCODER_ENABLE = no
